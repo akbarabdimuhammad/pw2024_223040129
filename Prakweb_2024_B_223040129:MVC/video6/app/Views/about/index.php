88b24fd135b21d6a576bd68c446e3be3c87d4d65
akbarabdimuhammad <125510670+akbarabdimuhammad@users.noreply.github.com>
2024-10-28 14:58:03 +0700

    Delete Prakweb_2024_B_223040129:MVC/video6 directory

--- Prakweb_2024_B_223040129:MVC/video6/app/Views/about/index.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-    <h1>About Me</h1>
-    <img src="<?= BASEURL; ?>/img/wle.jpg" alt="meow" width="200" class="shadow">
-    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah <?= $data['pekerjaan']; ?></p>
-</div>
--- Prakweb_2024_B_223040129:MVC/video6/app/core/constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/praktikumWeb/MVC/Video_6/public');
